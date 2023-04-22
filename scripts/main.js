74b91ac515d9d8146f0a7e1b5600e6b6eb88bd82
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2023-04-22 19:24:54 +0800

    updated color

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ Events.on(ClientLoadEvent, () => {
   Vars.content.liquids().each(c => c.localizedName = "[#" + c.color + "]" + c.localizedName);
   Vars.content.statusEffects().each(c => c.localizedName = "[#" + c.color + "]" + c.localizedName);
   Vars.content.getBy(ContentType.weather).each(c => c.localizedName = "[#" + c.color + "]" + c.localizedName);
-  Vars.content.planets().each(c => c.localizedName = "[#" + c.atmosphereColor + "]" + c.localizedName);
-  Vars.content.sectors().each(c => c.localizedName = "[#" + c.planet.atmosphereColor + "]" + c.localizedName);
+  Vars.content.planets().each(c => c.localizedName = "[#" + c.iconColor + "]" + c.localizedName);
+  Vars.content.sectors().each(c => c.localizedName = "[#" + c.planet.iconColor + "]" + c.localizedName);
   Vars.content.blocks().each(c => c.hasColor, c => c.localizedName = "[#" + c.mapColor + "]" + c.localizedName);
 });
